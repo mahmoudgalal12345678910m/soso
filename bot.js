827b75309261042f19fd50cce510576dcd583a5b
kingofmenewwork <43719252+mahmoudgalal12345678910m@users.noreply.github.com>
2018-11-06 09:37:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,46 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
 console.log("Welcome Again !");
  
- client.on("guildMemberRemove", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
-╔╦╦╦═╦╗╔═╦═╦══╦═╗
-║║║║╩╣╚╣═╣║║║║║╩╣
-╚══╩═╩═╩═╩═╩╩╩╩═╝ 
-السلام عليكم تعالى حق سيرفرنا الجيمر + المبرمجين وسهر وضحك منتظرينك يا قلبى :heart:
-:smile: فى انتظارك نلعب مع بعض 
-:tada: جيف اواى على حسابات ماين كرافت علطول 
- 
-https://discord.gg/khYKYCe
-                           
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`)
-}).catch(console.error)
 
-})
- 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
-╔╦╦╦═╦╗╔═╦═╦══╦═╗
-║║║║╩╣╚╣═╣║║║║║╩╣
-╚══╩═╩═╩═╩═╩╩╩╩═╝ 
-السلام عليكم تعالى حق سيرفرنا الجيمر + المبرمجين وسهر وضحك منتظرينك يا قلبى :heart:
-:smile: فى انتظارك نلعب مع بعض 
-:tada: جيف اواى على حسابات ماين كرافت علطول 
- 
-https://discord.gg/khYKYCe
-                           
-! OneFamily .                              
-الدعووة خاصة لك ي قلبي ... [ ${member}  ]
-**`) 
-}).catch(console.error)
-})
+          const devs = ['200644160179535872' , '' , '' , ''];
+          const adminprefix = "+";
+          client.on('message', message => {
+              var argresult = message.content.split(` `).slice(1).join(' ');
+                if (!devs.includes(message.author.id)) return;
+
+            if (message.content.startsWith(adminprefix + 'st')) {
+              client.user.setGame(argresult, "https://www.twitch.tv/ حب بلا حدود");
+                message.channel.sendMessage(`**✅   ${argresult}**`)
+            }
+            });
+
+
+             
 client.login(process.env.BOT_TOKEN);
 
